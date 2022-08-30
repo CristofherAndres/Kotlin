2f192d0d00de39481451587e02c6ff83c0b4ebb5
Cristofher <Cristofher.rojas@inacapmail.cl>
2022-08-30 10:37:51 -0400

    Kotlin

---
+++ variables.kt
@@ -0,0 +1,35 @@
+//Uso de variables
+
+fun main() {
+    println("Bienvenido a Kotlin")
+    
+    //La primera var
+    //Kotlin puede definir el tipo de dato (No tan recomendable)
+    var numeroElegido = 5
+    
+    //Nosotros indicar el tipo de dato
+    // Tipo entero
+    var num1:Int = 7
+    
+    // Doble del entero
+    var num2:Long = 75
+    
+    // Uso de flotante - 6 decimales
+    var num3:Float = 1.5f
+    
+    // Uso de double - 12 decimales
+    var num4:Double = 5.323
+    
+    //Variables de tipo Alfanúmericas
+    
+    //Para 1 letra ''
+    var txtLetra:Char = 'h'
+    
+    //Para 1 texto que usar ""
+    var txtLinea:String = "Hola"
+    
+    //Boleanos
+    var estoyAburrido:Boolean = false
+    var estoyFeliz:Boolean = true
+    
+}
